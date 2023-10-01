40e8962862629b0d63d26f265afd5ba5af5ecca4
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2023-10-01 20:38:01 +0100

    style: Navbar on scroll shadow

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -5,10 +5,23 @@ import React from 'react'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedinIn, } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const NavBar = () => {
   const [nav, setNav] = useState(false)
+  const [shadow, setShadow] = useState(false)
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true)
+
+      } else {
+        setShadow(false)
+      }
+    }
+    window.addEventListener('scroll', handleShadow)
+  }, [])
 
 
   const handleNav = () => {
@@ -16,27 +29,30 @@ const NavBar = () => {
   }
 
   return (
-    <div className='w-full h-20 shadow-xl z-[100]'>
+    <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16 '>
-        <Image src='/../assets/Blinxz.svg' alt='Blinxz logo' width='70' height='50' />
+        <Link href='/'>
+          <Image src='/../assets/Blinxz.svg' alt='Blinxz logo' width='70' height='50' />
+        </Link>
         <div>
           <ul className='hidden md:flex'>
-            <Link href=''>
+            <Link href='/'>
               <li className='ml-10 text-xs uppercase hover:border-b'>Home</li>
             </Link>
-            <Link href=''>
+            <Link href='/#about'>
               <li className='ml-10 text-xs uppercase hover:border-b'>about</li>
             </Link>
-            <Link href=''>
+            <Link href='/#skills'>
               <li className='ml-10 text-xs uppercase hover:border-b'>skills</li>
             </Link>
-            <Link href=''>
+            <Link href='/#project'>
               <li className='ml-10 text-xs uppercase hover:border-b'>projects</li>
             </Link>
-            <Link href=''>
+            <Link href='/#resume'>
               <li className='ml-10 text-xs uppercase hover:border-b'>resume</li>
             </Link>
-            <Link href=''>
+            <Link href='/#contact'>
               <li className='ml-10 text-xs uppercase hover:border-b'>contact</li>
             </Link>
           </ul>
@@ -45,6 +61,7 @@ const NavBar = () => {
           </div>
         </div>
       </div>
+
 
       <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ""}>
         <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:[45%] h-screen bg-[#ecf0f3] px-10 pt-5 ease-in duration-500' : 'fixed left-[-100%] top-0 px-10 pt-5 ease-in duration-500'}>
@@ -63,19 +80,19 @@ const NavBar = () => {
               <Link href='/'>
                 <li className='py-3 text-sm hover:scale-105 ease-in duration-500'>Home</li>
               </Link>
-              <Link href='/'>
+              <Link href='/#about'>
                 <li className='py-3 text-sm hover:scale-105 ease-in duration-500'>about</li>
               </Link>
-              <Link href='/'>
+              <Link href='/#skills'>
                 <li className='py-3 text-sm hover:scale-105 ease-in duration-500'>skills</li>
               </Link>
-              <Link href='/'>
+              <Link href='/#project'>
                 <li className='py-3 text-sm hover:scale-105 ease-in duration-500'>projects</li>
               </Link>
-              <Link href='/'>
+              <Link href='/#resume'>
                 <li className='py-3 text-sm hover:scale-105 ease-in duration-500'>resume</li>
               </Link>
-              <Link href='/'>
+              <Link href='/#contact'>
                 <li className='py-3 text-sm hover:scale-105 ease-in duration-500'>contact</li>
               </Link>
             </ul>
