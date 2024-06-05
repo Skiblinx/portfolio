88f76d63f567f55db94310efb28fff53788109ee
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2024-06-05 09:01:54 +0100

    projects updated

--- src/app/components/ProjectItem.jsx
+++ src/app/components/ProjectItem.jsx
@@ -5,13 +5,13 @@ import Link from 'next/link'
 const ProjectItem = ({ title, bgImage, framework, projectUrl }) => {
   return (
 
-    <div className='relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
       <Image className='rounded-xl group-hover:opacity-10' src={bgImage} alt='portfolio project' />
       <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-widest text-center'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center'>{framework}</p>
         <Link href={projectUrl}>
-          <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+          <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>Visit Site</p>
         </Link>
       </div>
     </div>
--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
 import portfolioImg from '../../../public/assets/portfolioImg.png'
-import fitnessImg from '../../../public/assets/fitnessImg.png'
-import taleem from '../../../public/assets/taleem-admin.png'
+import tailordom from '../../../public/assets/Tailordom.png'
+import quiickops from '../../../public/assets/Quiickops.png'
+import store from '../../../public/assets/store.png'
+import foodBag from '../../../public/assets/foodBag.png'
+import hikMind from '../../../public/assets/HikMind.png'
+import gtfinder from '../../../public/assets/gtfinder.png'
 import Link from 'next/link'
 import ProjectItem from './ProjectItem'
 
@@ -14,21 +18,42 @@ const Projects = () => {
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
+
+          <ProjectItem
+            title='Tailordom'
+            bgImage={tailordom}
+            framework='ReactJs/TypeScript'
+            projectUrl='https://www.tailordom.com/' />
+          <ProjectItem
+            title='QuiickOps'
+            bgImage={quiickops}
+            framework='ReactJs/TypeScript'
+            projectUrl='https://www.quiickops.com/' />
+          <ProjectItem
+            title='Falz Store'
+            bgImage={store}
+            framework='ReactJs/TypeScript/Redux'
+            projectUrl='https://bazar-admin-panel-ts.vercel.app/' />
+          <ProjectItem
+            title='FoodBag'
+            bgImage={foodBag}
+            framework=''
+            projectUrl='https://food-bag.vercel.app/' />
+          <ProjectItem
+            title='HikMinder'
+            bgImage={hikMind}
+            framework=''
+            projectUrl='https://hikminderr.vercel.app/' />
+          <ProjectItem
+            title='GitHub Profile Finder'
+            bgImage={gtfinder}
+            framework=''
+            projectUrl='https://github-finder-typescript-kzjx.vercel.app/' />
           <ProjectItem
             title='Portfolio Site'
             bgImage={portfolioImg}
             framework='Next Js'
             projectUrl='/portfolio' />
-          <ProjectItem
-            title='Fitness App'
-            bgImage={fitnessImg}
-            framework='React Js'
-            projectUrl='/fitnessApp' />
-          <ProjectItem
-            title='Mkan Taleem-Admin Dashboard'
-            bgImage={taleem}
-            framework='Next Js'
-            projectUrl='/taleem-admin' />
 
         </div>
       </div>
