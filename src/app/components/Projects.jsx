8a81fdef1f4865be6ac77bca27a82c34e4d81c4a
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2024-08-16 22:34:30 +0100

    feat: add new project details

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -7,7 +7,8 @@ import store from '../../../public/assets/store.png'
 import foodBag from '../../../public/assets/foodBag.png'
 import hikMind from '../../../public/assets/HikMind.png'
 import gtfinder from '../../../public/assets/gtfinder.png'
-import Link from 'next/link'
+import FlashChange from '../../../public/assets/flashChangee.png'
+// import Link from 'next/link'
 import ProjectItem from './ProjectItem'
 
 
@@ -23,37 +24,54 @@ const Projects = () => {
             title='Tailordom'
             bgImage={tailordom}
             framework='ReactJs/TypeScript'
-            projectUrl='https://www.tailordom.com/' />
+            projectUrl='https://www.tailordom.com/'
+            description='This is an online platform that allows users to create professional websites without coding. It features an intuitive drag and drop interface for easy website building.'
+          />
           <ProjectItem
             title='QuiickOps'
             bgImage={quiickops}
             framework='ReactJs/TypeScript'
+            description='This is an HR management platform that streamlines workforce operations for businesses of all sizes, offering tools for recruiting, onboarding, performance management, and employee engagement.'
             projectUrl='https://www.quiickops.com/' />
+          <ProjectItem
+            title='FlashChange'
+            bgImage={FlashChange}
+            framework='ReactJs/TypeScript'
+            description=' FlashChange is a platform for fast, secure crypto to fiat exchanges, supporting multiple cryptocurrencies with competitive rates and quick transactions'
+            projectUrl='https://flashchange.io/' />
           <ProjectItem
             title='Falz Store'
             bgImage={store}
             framework='ReactJs/TypeScript/Redux'
-            projectUrl='https://bazar-admin-panel-ts.vercel.app/' />
+            projectUrl='https://bazar-admin-panel-ts.vercel.app/'
+            description='A sleek and responsive admin dashboard for managing e-commerce activities. it simplifies product, order, and user management with real time analytics.'
+          />
           <ProjectItem
             title='FoodBag'
             bgImage={foodBag}
             framework=''
-            projectUrl='https://food-bag.vercel.app/' />
+            projectUrl='https://food-bag.vercel.app/'
+            description='An easy to use platform for ordering your favorite meals online, designed for simplicity and convenience. Perfect for quick, stress free food delivery'
+          />
           <ProjectItem
             title='HikMinder'
             bgImage={hikMind}
             framework=''
-            projectUrl='https://hikminderr.vercel.app/' />
+            projectUrl='https://hikminderr.vercel.app/'
+            description='A simple tool designed for students and lecturers to easily track and manage their daily classes. Stay organized and never miss a session.'
+          />
           <ProjectItem
             title='GitHub Profile Finder'
             bgImage={gtfinder}
             framework=''
+            description='Easily search for GitHub profiles and explore user details in a simple and streamlined interface. Built with TypeScript for a smooth experience.'
             projectUrl='https://github-finder-typescript-kzjx.vercel.app/' />
           <ProjectItem
             title='Portfolio Site'
             bgImage={portfolioImg}
             framework='Next Js'
-            projectUrl='https://blinxz.netlify.app/' />
+            projectUrl='https://blinxz.netlify.app/'
+          />
 
         </div>
       </div>
